027286287aa5ed79c75c70f1cf40b5fec8fb4a47
Heimer Martinez <hmartinez@cafetosoftware.com>
2022-10-02 07:44:23 -0500

    configracion del archivo webpack config con los modulos salida y plugins, se realizo la configuracion del template y demas

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	mode: 'development',
 
+	output: {
+		clean: true,
+	},
+
 	module: {
-		rules: [],
+		rules: [
+			{
+				test: /\.html$/,
+				loader: 'html-loader',
+				options: {
+					sources: false,
+				},
+			},
+		],
 	},
 
 	optimization: {},
 
-	plugins: [],
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Webpack app',
+			filename: 'index.html',
+			template: './src/index.html',
+		}),
+	],
 };
