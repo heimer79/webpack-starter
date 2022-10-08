5700353bc729a310509db3b4611519d9e6a7f76e
Heimer Martinez <hmartinez@cafetosoftware.com>
2022-10-08 13:02:36 -0500

    instalacion de mini extract plugin para cargar un css externo al html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -18,7 +19,12 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
+				exclude: /styles.css$/,
 				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /styles.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
 			},
 		],
 	},
@@ -30,6 +36,12 @@ module.exports = {
 			title: 'Webpack app',
 			filename: 'index.html',
 			template: './src/index.html',
+		}),
+
+		new MiniCssExtractPlugin({
+			//filename: '[name].[fullhash].css',
+			filename: '[name].css',
+			ignoreOrder: false,
 		}),
 	],
 };
